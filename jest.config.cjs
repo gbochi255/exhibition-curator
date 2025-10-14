b81657b95b1689e4afba92793c05dd99e6b17d9e
gbochi255 <126166418+gbochi255@users.noreply.github.com>
2025-10-14 19:17:15 +0100

    generated production ready bundle in dist/folder

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,19 +1,28 @@
+const path = require('path');
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
 
     transform: {
-        '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }]
+        '^.+\\.(ts|tsx)$': ['ts-jest', 
+            { 
+                tsconfig: '<rootDir>/tsconfig.jest.json' 
+            }]
     },
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 
     setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^axios$': require.resolve('axios')
     },
     globals: {
         'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.jest.json'
+            tsconfig: path.resolve(__dirname, 'tsconfig.jest.json'),
+            //tsconfig: '<rootDir>/tsconfig.jest.json'
+            diagnostics: true
         }
     },
     transformIgnorePatterns: ['/node_modules/'],
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 //import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
